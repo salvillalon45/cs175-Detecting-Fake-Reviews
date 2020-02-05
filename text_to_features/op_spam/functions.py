21a1811c2a31bf286e305fe2d6d976837f3e061f
Salvador Villalon <salvav1@uci.edu>
2020-02-05 13:42:34 -0800

    Creating function names

--- text_to_features/op_spam/functions.py
+++ text_to_features/op_spam/functions.py
@@ -15,81 +15,87 @@ from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 
-reviews = list()
-scores = list()
 
-# negative_polarity directory
-# ---------------------------------------------------------
-files_in_directory_negative_polarity = os.listdir("../../datasets/op_spam_v1.4/test_op_spam_v1.4/negative_polarity/")
-file_path_negative_polarity = "../../datasets/op_spam_v1.4/test_op_spam_v1.4/negative_polarity/"
+def create_reviews_scores_arrays():
+    # This function uses the op_spam dataset and extracts the reviews and the flag from the file name
+    # This is needed so that we can do vectorization. We plan on using CountVectorization
 
-# Loop over the files in negative_polarity directory
-# Open the file line by line
-for file_name in files_in_directory_negative_polarity:
-    print("The file is:: ", file_name)
-    print("File flag:: ", file_name[0])
-    file_flag = file_name[0]
-    file_path = file_path_negative_polarity + file_name
-    file_open = open(file_path)
-    review = file_open.readline()
-    print("The Review is:: ")
-    print(review)
-    print(" ")
-    print("The File Flag is:: ")
-    print(file_flag)
-    if file_flag == "d":
-        scores.append(file_flag)
-    else:
-        scores.append(file_flag)
+    reviews = list()
+    scores = list()
+    # negative_polarity directory
+    # ---------------------------------------------------------
+    files_in_directory_negative_polarity = os.listdir("../../datasets/op_spam_v1.4/test_op_spam_v1.4/negative_polarity/")
+    file_path_negative_polarity = "../../datasets/op_spam_v1.4/test_op_spam_v1.4/negative_polarity/"
+    
+    # Loop over the files in negative_polarity directory
+    # Open the file line by line
+    for file_name in files_in_directory_negative_polarity:
+        print("The file is:: ", file_name)
+        print("File flag:: ", file_name[0])
+        file_flag = file_name[0]
+        file_path = file_path_negative_polarity + file_name
+        file_open = open(file_path)
+        review = file_open.readline()
+        print("The Review is:: ")
+        print(review)
+        print(" ")
+        print("The File Flag is:: ")
+        print(file_flag)
+        if file_flag == "d":
+            scores.append(0)
+        else:
+            scores.append(1)
+    
+        reviews.append(review)
+    
+    # positive_polarity directory
+    # ---------------------------------------------------------
+    files_in_directory_positive_polarity = os.listdir("../../datasets/op_spam_v1.4/test_op_spam_v1.4/positive_polarity/")
+    file_path_positive_polarity = "../../datasets/op_spam_v1.4/test_op_spam_v1.4/positive_polarity/"
+    
+    # Loop over the files in positive_polarity directory
+    # Open the file line by line
+    for file_name in files_in_directory_positive_polarity:
+        print("The file is:: ", file_name)
+        print("File flag:: ", file_name[0])
+        file_flag = file_name[0]
+        file_path = file_path_positive_polarity + file_name
+        file_open = open(file_path)
+        review = file_open.readline()
+        print("The Review is:: ")
+        print(review)
+        if file_flag == "d":
+            scores.append(0)
+        else:
+            scores.append(1)
+    
+        reviews.append(review)
+    
+    
+    # print(reviews)
+    # print(scores)
+    
 
-    reviews.append(review)
-
-# positive_polarity directory
-# ---------------------------------------------------------
-files_in_directory_positive_polarity = os.listdir("../../datasets/op_spam_v1.4/test_op_spam_v1.4/positive_polarity/")
-file_path_positive_polarity = "../../datasets/op_spam_v1.4/test_op_spam_v1.4/positive_polarity/"
-
-# Loop over the files in positive_polarity directory
-# Open the file line by line
-for file_name in files_in_directory_positive_polarity:
-    print("The file is:: ", file_name)
-    print("File flag:: ", file_name[0])
-    file_flag = file_name[0]
-    file_path = file_path_positive_polarity + file_name
-    file_open = open(file_path)
-    review = file_open.readline()
-    print("The Review is:: ")
-    print(review)
-    if file_flag == "d":
-        scores.append(file_flag)
-    else:
-        scores.append(file_flag)
-
-    reviews.append(review)
-
-
-# print(reviews)
-# print(scores)
-
-vectorizer = CountVectorizer(ngram_range=(1,2), stop_words="english", min_df=0.01)
-
-# create a sparse BOW array from 'text' using vectorizer
-X = vectorizer.fit_transform(reviews)
-
-# an alternative above would be to use TfIDF rather than counts - which is very simple to do (but not needed here)
-print('Data shape: ', X.shape)
-
-print(X)
-
-corpus = [
-    'This is the first document.',
-    'This document is the second document.',
-    'And this is the third one.',
-     'Is this the first document?',
-]
-
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(corpus)
-print(X)
-print(X.toarray())
+def create_bow_from_reviews():
+    vectorizer = CountVectorizer(ngram_range=(1,2), stop_words="english", min_df=0.01)
+    
+    # create a sparse BOW array from 'text' using vectorizer
+    X = vectorizer.fit_transform(reviews)
+    
+    # an alternative above would be to use TfIDF rather than counts - which is very simple to do (but not needed here)
+    print('Data shape: ', X.shape)
+    
+    print(X)
+    
+    corpus = [
+        'This is the first document.',
+        'This document is the second document.',
+        'And this is the third one.',
+         'Is this the first document?',
+    ]
+    
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    print(X)
+    print(X.toarray())
 
