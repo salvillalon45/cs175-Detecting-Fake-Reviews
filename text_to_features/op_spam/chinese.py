f2021152b1e256cf28050fbeb0183ca2ec354524
Andrew Self <awself@uci.edu>
2020-02-12 14:05:14 -0800

    adding chinese processing

--- text_to_features/op_spam/chinese.py
+++ text_to_features/op_spam/chinese.py
@@ -9,15 +9,34 @@ def read_chinese():
     #     print(row)
 
     labels = []
-    text = []
+    reviews = []
 
     count = 0
     for line in open(file_name):
         count = count + 1
-        if count > 3:
-            break
+        if count == 0:
+            continue
         line = line.split(',', maxsplit=5) # max split equals 5 so as to not split
-        print(line)
+        # print(line)
+        label = line[1]
+        review = line[5]
+        # print(label)
+        # print(review)
+        if label == '+':
+            labels.append(0)
+        else:
+            labels.append(1)
+        reviews.append(review)
+
+    # print(len(labels), len(reviews))
+    # print(labels)
+    return labels, reviews
+
+def chinese_BOW(labels, reviews):
+    print('Creating BOW')
+
+
 
 if __name__ == '__main__':
-    read_chinese()
+    labels, reviews = read_chinese()
+    chinese_BOW(labels, reviews)
