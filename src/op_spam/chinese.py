8761a3f8912069d637f997bb4ebc6a8f2194ec8b
Andrew Self <awself@uci.edu>
2020-02-12 14:39:00 -0800

    adding more chinese processing

--- src/op_spam/chinese.py
+++ src/op_spam/chinese.py
@@ -1,5 +1,8 @@
 # Module to use chinese data
 import csv
+from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+import os
+
 
 def read_chinese():
     print('Reading Chinese')
@@ -13,9 +16,10 @@ def read_chinese():
 
     count = 0
     for line in open(file_name):
-        count = count + 1
+        count = count
         if count == 0:
             continue
+        count = count + 1
         line = line.split(',', maxsplit=5) # max split equals 5 so as to not split
         # print(line)
         label = line[1]
@@ -32,8 +36,18 @@ def read_chinese():
     # print(labels)
     return labels, reviews
 
+
+
 def chinese_BOW(labels, reviews):
     print('Creating BOW')
+    # seg = StanfordSegmenter('../../datasets/data-hauyi/stanford-segmenter-2018-10-16')
+    os.environ["STANFORD_SEGMENTER"] = '../../datasets/data-hauyi/stanford-segmenter-2018-10-16'
+    seg = StanfordSegmenter('../../datasets/data-hauyi/stanford-segmenter-2018-10-16/stanford-segmenter-3.9.2.jar')
+    seg.default_config('zh',)
+    for i in reviews:
+        print(i)
+        print(seg.segment(i))
+    
 
 
 
