689c17e9a2a66911ce51584e0ba8bdd38c4263c6
Andrew Self <awself@uci.edu>
2020-02-28 12:19:51 -0800

    andrew chinese update

--- src/chinese.py
+++ src/chinese.py
@@ -2,24 +2,32 @@
 import csv
 from nltk.tokenize.stanford_segmenter import StanfordSegmenter
 import os
+import functions
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def read_chinese():
     print('Reading Chinese')
-    file_name = '../../datasets/data-hauyi/ICDM_REVIEWS_TO_RELEASE_encoding=utf-8.csv'
-    reader = csv.reader(file_name, delimiter=',')
+    file_name = '../datasets/data-hauyi/ICDM_REVIEWS_TO_RELEASE_encoding=utf-8.csv'
+
+    # reader = csv.reader(file_name, delimiter=',')
     # for row in reader:
     #     print(row)
+
+    # for line in open('../requirements.txt'):
+    #     print(line)
 
     labels = []
     reviews = []
 
     count = 0
     for line in open(file_name):
-        count = count
-        if count == 0:
-            continue
+        print('HERE')
+        # count = count
         count = count + 1
+        if count == 1:
+            continue
+
         line = line.split(',', maxsplit=5) # max split equals 5 so as to not split
         # print(line)
         label = line[1]
@@ -32,25 +40,62 @@ def read_chinese():
             labels.append(1)
         reviews.append(review)
 
+        if count > 5:
+            break
+
     # print(len(labels), len(reviews))
     # print(labels)
     return labels, reviews
 
 
 
-def chinese_BOW(labels, reviews):
+def segment(labels, reviews):
+
+    segmented = []
+
     print('Creating BOW')
     # seg = StanfordSegmenter('../../datasets/data-hauyi/stanford-segmenter-2018-10-16')
-    os.environ["STANFORD_SEGMENTER"] = '../../datasets/data-hauyi/stanford-segmenter-2018-10-16'
-    seg = StanfordSegmenter('../../datasets/data-hauyi/stanford-segmenter-2018-10-16/stanford-segmenter-3.9.2.jar')
+    os.environ["STANFORD_SEGMENTER"] = '../datasets/data-hauyi/stanford-segmenter-2018-10-16'
+    seg = StanfordSegmenter('../datasets/data-hauyi/stanford-segmenter-2018-10-16/stanford-segmenter-3.9.2.jar')
     seg.default_config('zh',)
+    count = 0
+
     for i in reviews:
         print(i)
-        print(seg.segment(i))
+        s = seg.segment(i)
+        print(s)
+        segmented.append(s)
+        # print('Tokenize: ')
+        # print(seg.tokenize(s))
+        count = count + 1
+        if count > 5:
+            break
     
+    return(segmented)
 
-
+def chinese_BOW(reviews):
+    # vectorizer = CountVectorizer(ngram_range=(1,2), stop_words="english", min_df=0.01)
+    vectorizer = CountVectorizer()
+    print(reviews)
+    X = vectorizer.fit_transform(reviews)
+    return X, vectorizer
 
 if __name__ == '__main__':
     labels, reviews = read_chinese()
-    chinese_BOW(labels, reviews)
+    # print(labels,reviews)
+    reviews = segment(labels, reviews)
+    print(reviews)
+    BOW, vec = chinese_BOW(reviews)
+    print('BOW: ')
+    print(BOW)
+    print('BOW to array: ')
+    print(BOW.toarray())
+    print('shape: ')
+    print(BOW.shape)
+    print('get feature names')
+    print(vec.get_feature_names())
+    print('get params')
+    print(vec.get_params())
+    print('get stop words')
+    print(vec.get_stop_words())
+
